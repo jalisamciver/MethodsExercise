9f4be2f4903972c027f5c58aa2370dc9eb36ad09
Jalisa McIver <jalisamciver@gmail.com>
2023-02-01 17:04:50 -0500

    completed main method

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace MethodsExercise
+﻿using System;
+using Internal;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //-------------------------Exercise 1-------------------------
+            //Name: Jalisa
+            //Favorite Color: Purple
+            //Favorite Animal: Red Pandas
+            //Favorite Band: Paramore
+
+            Console.WriteLine("Hello - what is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. What is your favorite color");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awesome color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! Now, what is your favorite band?");
+            var band = Console.ReadLine();
         }
     }
 }
